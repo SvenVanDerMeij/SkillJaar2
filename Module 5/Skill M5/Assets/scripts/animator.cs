aa874fd6af9ab6d49b3d3cfa14ee95cc726ed453
SvenVanDerMeij <38007@ma-web.nl>
2024-09-13 11:05:49 +0200

    eerste skill opdracht

---
+++ Module 5/Skill M5/Assets/scripts/animator.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class animator : MonoBehaviour
+{
+    private Animator animate;
+    public float speed;
+    private float inputs = 1;
+    private float secondsPast = 1;
+    static public bool started = false;
+    static public bool finished = false;
+    public static bool won = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animate = GetComponent<Animator>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 80)
+        {
+            finished = true;
+            animate.SetTrigger("won");
+            animate.ResetTrigger("runStart");
+            animate.ResetTrigger("runStop");
+            won = true;
+
+        }
+        secondsPast += 1 * Time.deltaTime;
+       // Debug.Log( speed);
+        if (speed > 1)
+        {
+            animate.SetTrigger("runStart");
+            animate.ResetTrigger("runStop");
+        }else if (speed <= 1)
+        {
+            animate.SetTrigger("runStop");
+            animate.ResetTrigger("runStart");
+        }
+        transform.position = transform.position += new Vector3(speed * Time.deltaTime, 0,0 );
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            started = true;
+            inputs += 1;
+            
+            
+            
+        }
+       if (started == true && finished == false)
+        {
+
+            speed = 2*(inputs / secondsPast);
+
+            if (speed >= 20)
+            {
+                inputs = secondsPast * 10;
+            }
+
+            if (speed < 1)
+            {
+                inputs = 0;
+            }
+
+        } else if (started == false && finished == false)
+        {
+            speed = 0;
+        } else if (started == true && finished == true)
+        {
+            speed = 0;
+        }
+       if (timer.time < 0)
+        {
+            speed = 0;
+        }
+        
+            
+        
+        
+
+    }
+    public void heartAttack()
+    {
+        animate.SetTrigger("lost");
+        animate.ResetTrigger("runStart");
+        animate.ResetTrigger("runStop");
+    }
+}
---
+++ Module 5/Skill M5/Assets/scripts/timer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    
+    public animator script;
+    static public float time = 8;
+    private TMP_Text scoreField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreField = GetComponent<TMP_Text>();
+        script = GameObject.Find("player").GetComponent<animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreField.text = "" + Mathf.RoundToInt(time);
+        if (animator.started == true && animator.won == false)
+        {
+            time -= 1 * Time.deltaTime;
+        } else if (animator.started == true && animator.won == true)
+        {
+            time += 0;
+        }
+
+        if (time < 0)
+        {
+            animator.finished = true;
+            script.heartAttack();
+        }
+        
+    }
+}
