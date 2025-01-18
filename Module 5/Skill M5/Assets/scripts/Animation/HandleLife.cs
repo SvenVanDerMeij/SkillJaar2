3ba50179e368fcfaf364ad0b31423d1d4c473c14
SvenVanDerMeij <38007@ma-web.nl>
2025-01-18 19:11:24 +0100

    animation opdracht

---
+++ Module 5/Skill M5/Assets/scripts/Animation/HandleLife.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleLife : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private int lives = 5;
+    public static event Action onHeroDeath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Delete))
+        {
+            lives--;
+            animator.SetInteger("lives", lives);
+            if (lives == 0)
+            {
+                onHeroDeath?.Invoke();
+            }
+        }
+    }
+}
